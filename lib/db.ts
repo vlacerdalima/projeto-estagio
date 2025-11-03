debc37b5105660ab1816503e019c06f30473907b
Vitor Lacerda <vlacerdalima@gmail.com>
2025-11-03 04:51:10 -0300

    fix: ajuste para conexão do bd

--- lib/db.ts
+++ lib/db.ts
@@ -1,12 +1,16 @@
 import { Pool } from 'pg';
 
-const pool = new Pool({
-  host: process.env.DB_HOST || 'localhost',
-  port: parseInt(process.env.DB_PORT || '5432'),
-  database: process.env.DB_NAME || 'seu_banco_de_dados',
-  user: process.env.DB_USER || 'seu_usuario',
-  password: process.env.DB_PASSWORD || 'sua_senha',
-});
+const pool = new Pool(
+  process.env.DATABASE_URL
+    ? { connectionString: process.env.DATABASE_URL }
+    : {
+        host: process.env.DB_HOST || 'localhost',
+        port: parseInt(process.env.DB_PORT || '5432'),
+        database: process.env.DB_NAME || 'seu_banco_de_dados',
+        user: process.env.DB_USER || 'seu_usuario',
+        password: process.env.DB_PASSWORD || 'sua_senha',
+      }
+);
 
 export default pool;
 
